c37781beeb6e9a436c0ba47c0fa13f0ebc79721f
balbaal <hai.balbaal@gmail.com>
2019-02-16 15:07:55 +0700

    connect to mongodb

---
+++ config/db.js
@@ -0,0 +1,8 @@
+// const user = ""
+// const password = ""
+// const host = ""
+
+module.exports = {
+    // mongoURI: `mongodb://${user}:${password}@${host}`
+    mongoURI: "mongodb://localhost:27017/mern_stack_app"
+}
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+// Connect To MongoDB
+const db = require('./config/db').mongoURI
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB is Connected . . .'))
+	.catch(err => console.log(`Something Wrong Connecting MongoDB : ${err}`))
+
+// Middleware Body Parsing
+app.use(bodyParser.json())
+
+// Require Routes
+
 
 // End Point API List
 app.get('/', (req, res) => {
