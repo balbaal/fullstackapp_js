b8a4eaac5489e56098872094e0c6e0676991e2bf
balbaal <hai.balbaal@gmail.com>
2019-03-02 06:27:37 +0700

    update routing api

--- routes/api/student.js
+++ routes/api/student.js
@@ -18,8 +18,8 @@ router.post("/", (req, res) => {
 
   newStudent
     .save()
-    .then(result => res.status(201).json(result))
-    .catch(err => res.json(err).status(400));
+    .then(result => res.status(201).json({message: result, success: true}))
+    .catch(err => res.json({message: "failed to make new student", success: false}).status(400));
 });
 
 // @route     GET api/student
@@ -29,9 +29,9 @@ router.get("/", (req, res) => {
   StudentModel.find()
     .then(result => {
       if (result.length > 0) {
-        res.json(result).status(200);
+        res.json({message: result, success: true}).status(200);
       } else {
-        res.json({ message: "Data Not Available" }).status(200);
+        res.json({ message: "Data Not Available", success: false }).status(404);
       }
     })
     .catch(err => res.json(err).status(500));
@@ -43,10 +43,9 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   StudentModel.findById(req.params.id)
     .then(result => {
-      res.send(result).status(200);
-      console.log(result);
+      res.json({message: result, success: true}).status(200);
     })
-    .catch(err => res.send(err).status(400));
+    .catch(err => res.status(404).json({success: false, message: `Id ${req.params.id} not found`}));
 });
 
 // @route   PUT api/student/:id
@@ -54,8 +53,8 @@ router.get("/:id", (req, res) => {
 // @access  Public
 router.put("/:id", (req, res) => {
   StudentModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then(result => res.send(result).status(200))
-    .catch(err => res.send(err).status(400));
+    .then(result => res.json({message: result, success: true}).status(200))
+    .catch(err => res.json({message: `update id : ${req.params.id} failed`, success: false}).status(400));
 });
 
 // @route DELETE api/student/:id
@@ -63,8 +62,8 @@ router.put("/:id", (req, res) => {
 // @access Public
 router.delete("/:id", (req, res) => {
   StudentModel.findByIdAndDelete(req.params.id)
-    .then(() => res.status(200).json({ success: true }))
-    .catch(err => res.status(404).json({ success: false }));
+    .then(() => res.status(200).json({ message: `success delete id : ${req.params.id}`, success: true }))
+    .catch(err => res.status(404).json({ message: `failed to update id : ${req.params.id}`, success: false }));
 });
 
 module.exports = router;
